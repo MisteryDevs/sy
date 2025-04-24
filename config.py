557f9f67119e400673342ddcf677bf17d010f2cd
MisteryDevs <officialbotdev@gmail.com>
2025-04-24 09:41:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,19 +31,19 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/RahulTC
+API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/Rishu1286
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MisteryDevs/AviaxMusic",
+    "https://github.com/MisteryDevs/pr",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ur_rishu_143")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ur_support07")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_ABOUT_PRINCE_l")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ll_TARON_KA_SHEHER_ll")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
