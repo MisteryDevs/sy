ceea02633d594590dee50711db956e53a6e53400
MisteryDevs <officialbotdev@gmail.com>
2025-04-25 10:34:40 +0530

    Update couple.py

--- AviaxMusic/plugins/tools/couple.py
+++ AviaxMusic/plugins/tools/couple.py
@@ -72,16 +72,16 @@ async def ctest(_, message):
          try:
             p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
          except Exception:
-            p1 = "RishuMusic/assets/upic.png"
+            p1 = "AviaxMusic/assets/upic.png"
          try:
             p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
          except Exception:
-            p2 = "RishuMusic/assets/upic.png"
+            p2 = "AviaxMusic/assets/upic.png"
 
          img1 = Image.open(f"{p1}")
          img2 = Image.open(f"{p2}")
 
-         img = Image.open("RishuMusic/assets/cppic.png")
+         img = Image.open("AviaxMusic/assets/cppic.png")
 
          img1 = img1.resize((390, 390))
          img2 = img2.resize((390,390))
